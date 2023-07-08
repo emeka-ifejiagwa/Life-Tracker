5cd5e41edc494914d6923d557d10bc866646e8ef
Chukwuemeka Ifejiagwa <cifejiagwa@salesforce.com>
2023-07-07 23:30:28 -0700

    Database URL updated

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const getDatabaseURI = () =>
-  `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+const getDatabaseURI = () => process.env.DATABASE_URL
+ 
+// local database URL: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 
 module.exports = getDatabaseURI;
